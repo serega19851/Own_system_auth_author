e50a009175c437beb55e14d85bdff0f2a335628d
sergeyyyy1985.an@gmail.com <sergeyyyy1985.an@gmail.com>
2025-09-18 11:46:15 +0300

    Add setings

---
+++ app/database.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+Base = declarative_base()
